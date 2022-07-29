1fa58d5b70cabb40e6bbeca57dfdfd8b64bdc6c6
AndreMBarreira <andremilla.am@gmail.com>
2022-07-29 19:29:10 -0300

    Category findById

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,12 @@ public class CategoryService {
 		//for (Category cat : list) {
 		//	listDTO.add(new Category(cat));}
 		//return listDTO
+	}
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 	
 }
